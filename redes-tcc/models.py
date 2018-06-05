74116a9433f9680fde9ca95d1471a882a0022e07
Nicoli Araujo <npa.matsu@gmail.com>
2018-06-05 16:53:15 -0400

    alexnet

--- redes-tcc/models.py
+++ redes-tcc/models.py
@@ -1,7 +1,9 @@
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.core import Activation, Flatten, Dense
-
+from keras.layers.normalization import BatchNormalization
+from keras import backend as K
+import warnings
 
 class Conv2Net:
     def __init__(self, width, height, depth, classes, weights_path=None):
@@ -48,4 +50,47 @@ class Conv2NetRegressor:
             model.load_weights(weights_path)
             
         self.model = model
-         
+
+class AlexNet:
+    def __init__(self, n_classes, weights_path=None):
+        self.model = self.build_model(n_classes, weights_path)
+        
+    def build_model(self, n_classes, weights_path=None):
+        if K.image_data_format()=='channels_first':
+            input_shape=(3, 227,227)
+        elif K.image_data_format()=='channels_last':
+            input_shape=(227,227,3)
+                
+        model = Sequential()
+
+        model.add(Conv2D(96, (11,11), strides=(4,4), input_shape=input_shape, padding='same', activation='relu', name='conv_1'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid'))
+
+        model.add(Conv2D(256, (5,5), strides=(1,1), padding='same', activation='relu', name='conv_2'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid', name='convpool_1'))
+
+        model.add(Conv2D(384, (3,3), strides=(1,1), padding='same', activation='relu', name='conv_3'))
+        
+        model.add(Conv2D(384, (3,3), strides=(1,1), padding='same', activation='relu', name='conv_4'))
+        
+        model.add(Conv2D(256, (3,3), strides=(1,1), padding='same', activation='relu', name='conv_5'))
+        model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='valid', name='convpool_5'))
+
+        model.add(Flatten(name='flatten'))
+        model.add(Dense(4096, activation='relu', name='dense_1'))
+        model.add(Dense(4096, activation='relu', name='dense_2'))
+        
+        model.add(Dense(n_classes, name='dense_3'))
+        
+        if n_classes==1:
+            warnings.warn('Considering a regression task with output function being \'relu\'')
+            model.add(Activation('relu', name='relu'))
+        else:
+            model.add(Activation('softmax', name='softmax'))
+            
+        if weights_path:
+            model.load_weights(weights_path, by_name=True)
+
+        return model
